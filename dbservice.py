ba3bcdaf1c986a87539254e85fc5d18317daca8e
Ivymaundu <ivymaundu03@gmail.com>
2023-09-25 18:11:16 +0300

    insert task

--- dbservice.py
+++ dbservice.py
@@ -1,15 +1,45 @@
 import psycopg2
-def get_data(my_data):
+try:
     conn = psycopg2.connect(
-    database="myduka_class", user='postgres', password='12345', host='127.0.0.1', port= '5432')
-    cursor = conn.cursor()
-    t = "select * from "+my_data
-    cursor.execute(t)
-    data = cursor.fetchall()
-    conn.close()
-    return data
+        database="myduka_class", user='postgres', password='12345', host='127.0.0.1', port= '5432')
+except:
+    print('unable to connect to the database')
 
-prods=get_data('products')
-sales=get_data('sales')
-print(prods)
-print(sales)
+# def get_data(my_data):
+#     cur = conn.cursor()
+#     t = "select * from "+my_data
+#     cur.execute(t)
+#     data = cur.fetchall()
+#     return data
+# prods=get_data('products')
+# sales=get_data('sales')
+# print(prods)
+# print(sales)
+
+
+def insert_records():
+    cur = conn.cursor()
+
+        # Execute the INSERT statement
+    cur.execute("INSERT INTO products (name, buying_price,selling_price,stock_quantity) VALUES (%s, %s,%s,%s)", ("yoghurt", 20,50,10))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print("Data inserted successfully!")  
+   
+
+total=insert_records()
+print(total)
+conn.close()
+
+#     cur=conn.cursor 
+#     new_record= f"INSERT INTO products {table_name} VALUES (%,%,%,%)"
+#     record_data=values
+#     cur.execute(new_record,record_data)
+#     data=all()
+#     return data
+
+# new_record=insert_records('products',"'downy',10,50,30")
+# print(new_record)
+
---
+++ insert.py
@@ -0,0 +1,32 @@
+import psycopg2
+try:
+    conn = psycopg2.connect(
+        database="myduka_class", user='postgres', password='12345', host='127.0.0.1', port= '5432')
+except:
+    print('unable to connect to the database')
+
+   
+
+def insert_records_into_table(table_name,values):
+    cursor=conn.cursor
+    task = f"INSERT INTO {table_name}(name,buying_price,selling_price,stock_quantity) VALUES('%',%,%,%)"
+    new_record=values
+    cursor.execute(task,new_record)
+    conn.commit()
+
+
+print('records inserted succesfully')
+conn.close
+
+def insert_records_into_table(table_name,values):
+    cursor=conn.cursor
+    task = f"INSERT INTO {table_name}(pid,quantity,created_at) VALUES(%,%,%)"
+    new_record=values
+    cursor.execute(task,new_record)
+    conn.commit()
+
+
+print('records inserted succesfully')
+conn.close
+
+    
