971c8888e61990aab9a4138d5d2f2b25d0785b30
Ivymaundu <ivymaundu03@gmail.com>
2023-10-05 23:46:11 +0300

    push

--- dbservice.py
+++ dbservice.py
@@ -1,5 +1,5 @@
-
 import psycopg2
+from flask import request,redirect
 try:
     conn = psycopg2.connect(
         database="myduka_class", user='postgres', password='12345', host='127.0.0.1', port= '5432')
@@ -14,27 +14,37 @@ def get_data(my_data):
     return data
 prods=get_data('products')
 sales=get_data('sales')
-print(prods)
-# print(sales)
 
-
-def remaining_stock():
-    stock = [] 
+def add_product(values):
     cur = conn.cursor()
-    
-    cur.execute(f'SELECT * FROM rem_stock')
-    rem_stocks = cur.fetchall()
+    insert_product="INSERT INTO products(name,buying_price,selling_price,stock_quantity) values(%s,%s,%s,%s)"
+    cur.execute(insert_product,values)
+    conn.commit()
 
-    for rem_stock in rem_stocks:
-        product = {}  # Create a new dictionary for each product
-        product['name'] = rem_stock[1]
-        product['rem_stock'] = rem_stock[2]
-        stock.append(product)
+def add_sale(values):
+    cur=conn.cursor()
+    insert_sale="INSERT INTO sales(pid,quantity,created_at) values(%s,%s,now())"
+    cur.execute(insert_sale,values)
+    conn.commit()
 
-    return stock
 
-s = remaining_stock()
-# the output is a list of dictionaries
-print(s)
 
-conn.close()
+# def calc_profit():
+#     cursor = conn.cursor()
+#     m =" SELECT SUM((products.selling_price - products.buying_price) * sales.quantity) AS profit,\
+#     sales.created_at FROM sales JOIN products ON sales.pid = products.id  WHERE sales.created_at >=\
+#     '2022-10-13 05:00:48'  AND sales.created_at <= '2023-10-05 20:33:31' GROUP BY sales.created_at\
+#     ORDER BY sales.created_at ASC;"
+
+#     cursor.execute(m)
+#     data3=cursor.fetchall()
+#     return data3
+def calc_profit():
+    cur=conn.cursor()
+    find_profit="SELECT date(created_at) as days,sum((products.selling_price-products.buying_price)*sales.quantity) as profit from sales join products on products.id=sales.pid group by days order by days;"
+    cur.execute(find_profit)
+    profit_per_day=cur.fetchall()
+    return profit_per_day
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from flask import Flask,render_template
+import psycopg2
+from flask import Flask,render_template,request,redirect
+from dbservice import get_data,add_product,add_sale,calc_profit
+
+try:
+    conn = psycopg2.connect(
+        database="myduka_class", user='postgres', password='12345', host='127.0.0.1', port= '5432')
+except:
+    print('unable to connect to the database')
+
 app=Flask(__name__)
 
 @app.route("/")
@@ -6,10 +15,45 @@ def myindex():
     return render_template("index.html")
 
 @app.route("/dashboard")
-def my_dashboard():
-    return render_template("dashboard.html")
+def profit1():       
+    dates=[str(i[0]) for i in calc_profit()]
+    profits=[float(i[1]) for i in calc_profit()]
 
-@app.route("/products")
-def my_dashboard():
-    return render_template("products.html")
-app.run()
+    return render_template("dashboard.html", dates = dates, profits=profits)
+
+
+# @app.route("/add-products" ,methods = ["POST"])
+# def add_products():
+#     name=request.form["name"]
+#     buying_price=request.form["buying_price"]
+#     selling_price=request.form["selling_price"]
+#     stock_quantity=request.form["stock_quantity"]
+#     values=(name,buying_price,selling_price,stock_quantity)
+#     add_product(values)
+#     return redirect("/products")
+
+
+
+# @app.route("/products")
+# def products():
+#     myprods=get_data("products")
+#     return render_template("products.html", myprods=myprods)
+
+
+# @app.route("/add-sale",method="POST")
+# def add_sale(values):
+#     pid=request.form["pid"]
+#     quantity=request.form["quantity"]
+#     values(pid,quantity)
+#     add_sale(values)
+#     return redirect("/sales")
+
+# @app.route("/sales")
+# def sales():
+#     products = get_data('products') 
+#     sales=get_data('sales')
+
+#     return render_template("sales.html", myprods=products,mysales=sales)
+
+
+app.run(debug=True)
